40678007cebda190d00dc84297a0526473c9e988
Jordan Corley <rainwolfj@gmail.com>
2016-03-08 16:20:15 -0600

    Base program appears complete, ideally a repeat function will be added, but may be pointless if expanded to epic

--- ropasc.rb
+++ ropasc.rb
@@ -6,9 +6,23 @@ armaments = ["rock", "paper", "scissors"]
 bot_weapon = armaments[rand(0..2)]
 
 # Comparison and Victory
-if (armaments.upcase == ("ROCK") && bot_weapon.upcase == ("SCISSORS")) ||
-
+if (weapon.upcase == ("ROCK") && bot_weapon.upcase == ("SCISSORS")) ||
+  (weapon.upcase == ("SCISSORS") && bot_weapon.upcase == ("PAPER")) ||
+  (weapon.upcase == ("PAPER") && bot_weapon.upcase == ("ROCK"))
+  puts "Player: " + weapon.upcase + " vs Computer: " + bot_weapon.upcase + " ----- PLAYER WINS"
+else
+  puts "Player: " + weapon.upcase + " vs Computer: " + bot_weapon.upcase + " ----- COMPUTER WINS"
+end
 # Print out results
-puts weapon
-puts bot_weapon
+# puts weapon
+# puts bot_weapon
 # Restart
+
+#Prototyping for later
+
+# class EquipBot
+#   def initialize()
+#     armaments = ["rock", "paper", "scissors"]
+#     bot_weapon = armaments[rand(0..2)]
+#   end
+# end
