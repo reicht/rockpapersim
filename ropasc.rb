8701dcef18d8310f4c618ab14cd7159b81cbb774
Jordan Corley <rainwolfj@gmail.com>
2016-03-08 21:17:06 -0600

    Purged relics, automation and information collection in place.  Removing verbose process, enabling requested test size

--- ropasc.rb
+++ ropasc.rb
@@ -1,35 +1,18 @@
-#  FUNCTIONAL EXHIBIT OF ORIGINAL
-#
-# puts "Select: (Rock) (Paper) (Scissors)"
-# weapon = gets.chomp
-#
-# armaments = ["rock", "paper", "scissors"]
-# bot_weapon = armaments[rand(0..2)]
-#
-#
-# if (weapon.upcase == ("ROCK") && bot_weapon.upcase == ("SCISSORS")) ||
-#   (weapon.upcase == ("SCISSORS") && bot_weapon.upcase == ("PAPER")) ||
-#   (weapon.upcase == ("PAPER") && bot_weapon.upcase == ("ROCK"))
-#   puts "Player: " + weapon.upcase + " vs Computer: " + bot_weapon.upcase + " ----- PLAYER WINS"
-# else
-#   puts "Player: " + weapon.upcase + " vs Computer: " + bot_weapon.upcase + " ----- COMPUTER WINS"
-# end
-#
-
-#Prototyping for later
-
 class RockPaperSim
   def initialize(test_number)
     @test_number = test_number
     @armaments = ["rock", "paper", "scissors"]
     @left_weapon = " "
     @right_weapon = " "
+    @win_chart = {Left: 0, Right: 0, Tie: 0, Rock: 0, Paper: 0, Scissors: 0}
+
   end
   def run
     @test_number.times do
       rearm
       contest
     end
+    puts @win_chart
   end
   def rearm
     @left_weapon = @armaments[rand(0..2)]
@@ -40,8 +23,30 @@ class RockPaperSim
       (@left_weapon.upcase == ("SCISSORS") && @right_weapon.upcase == ("PAPER")) ||
       (@left_weapon.upcase == ("PAPER") && @right_weapon.upcase == ("ROCK"))
       puts "Left: " + @left_weapon.upcase + " vs Right: " + @right_weapon.upcase + " ----- RIGHT WINS"
+      @win_chart.each_key do |weaponchecker|
+        if weaponchecker.to_s.upcase == @right_weapon.upcase
+          @win_chart[weaponchecker] +=1
+        end
+      end
+      @win_chart[:Right] +=1
+    elsif (@right_weapon.upcase == ("ROCK") && @left_weapon.upcase == ("SCISSORS")) ||
+      (@right_weapon.upcase == ("SCISSORS") && @left_weapon.upcase == ("PAPER")) ||
+      (@right_weapon.upcase == ("PAPER") && @right_weapon.upcase == ("ROCK"))
+      puts "Left: " + @left_weapon.upcase + " vs Right: " + @right_weapon.upcase + " ----- LEFT WINS"
+      @win_chart.each_key do |weaponchecker|
+        if weaponchecker.to_s.upcase == @left_weapon.upcase
+          @win_chart[weaponchecker] +=1
+        end
+      end
+      @win_chart[:Left] +=1
     else
-      puts "Right: " + @left_weapon.upcase + " vs Left: " + @right_weapon.upcase + " ----- LEFT WINS"
+      puts "Left: " + @left_weapon.upcase + " vs Right: " + @right_weapon.upcase + " -----  TIE BETWEEN"
+      @win_chart.each_key do |weaponchecker|
+        if weaponchecker.to_s.upcase == @right_weapon.upcase
+          @win_chart[weaponchecker] +=1
+        end
+      end
+      @win_chart[:Tie] +=1
     end
   end
 end
